4116d88cc847ecd46d004c40e4c41983dd1055ee
arinmsn <arinmsn@gmail.com>
2019-09-06 13:07:21 -0700

    Did not eject (CSS Modules). CSS styling works fine.

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -1,26 +1,9 @@
 import React from "react";
-import { css } from "glamor";
+import "../css/styles.css";
 
 const NewsItem = ({ item }) => {
-  let news_item = css({
-    padding: "20px",
-    boxSizing: "border-box",
-    borderBottom: "1px solid grey",
-    ":hover": {
-      color: "purple",
-      fontWeight: "bold"
-    },
-    "@media(max-width: 500px)": {
-      color: "red"
-    }
-  });
-
-  let item_grey = css({
-    background: "lightgrey"
-  });
-
   return (
-    <div className={`${news_item} ${item_grey}`}>
+    <div className="news_item">
       <h3>{item.title}</h3>
       <div>{item.feed}</div>
     </div>
