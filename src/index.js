17eca6611b447c2b8ecdbc6b9f2c6d82114ec7eb
arinmsn <arinmsn@gmail.com>
2019-09-06 05:35:08 -0700

    Some CSS Styling and even handler simplification

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//  Components
+import Header from "./components/header";
+
 const App = () => {
   return (
-    <div className="newElement">
-      <h1>Hello, world!</h1>
-      <h2>I am doing great!</h2>
+    <div>
+      <Header />
     </div>
   );
 };
